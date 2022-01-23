0d034562f1a86307a9658173e82c65e388508f48
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-01-23 18:48:56 +0100

    circles renamed

--- circles/sketch.js
+++ circles/sketch.js
@@ -3,6 +3,7 @@ var L = 1e4;
 function drawCircles() {
   var circles = [];
   var limit = L;
+
   while (circles.length < 500 && limit > -1) {
     var circle = {
       x: random(width),
@@ -10,10 +11,9 @@ function drawCircles() {
       r: random(random(2, 7), 64),
     };
     let overlapped = false;
-    for (let other of circles) {
+    for (let other of circles)
       if (circle.r + other.r > dist(circle.x, circle.y, other.x, other.y))
         overlapped = true;
-    }
     if (!overlapped) circles.push(circle);
     limit--;
   }
@@ -23,6 +23,7 @@ function drawCircles() {
     ellipse(c.x, c.y, c.r * 2, c.r * 2);
   }
 }
+
 function setup() {
   createCanvas(720, 720);
   //background(0);
