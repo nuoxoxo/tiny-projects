fdc861a5934c45cca645b32a1b82e693162c5d79
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-01-28 21:28:47 +0100

    + mandelbrot set

---
+++ mandelbrot_set/sketch.js
@@ -0,0 +1,45 @@
+function setup()
+{
+    createCanvas(3200, 3200)
+    pixelDensity(1)
+    loadPixels()
+
+    const Finess = 64
+    const Maxitr = 100
+
+    for (let x = 0; x < width; x ++)
+    {
+        for (let y = 0; y < height; y ++)
+        {
+            let a = map(x, 0, width, -2, 2)
+            let b = map(y, 0, height, -2, 2)
+            let ca = a
+            let cb = b
+            let n = 0
+
+            while (n < Maxitr)
+            {
+                let temp_a = a * a - b * b
+                let temp_b = a * b * 2
+
+                a = ca + temp_a
+                b = cb + temp_b
+                
+                if (a + b > Finess) break
+                n ++
+            }
+
+            //let yanse = n === Maxitr ? 0 : 255
+            let yanse = n === Maxitr ? 0 : map(n, 0, Finess, 0, 255)
+
+            let px = (x + y * width) * 4
+            pixels[px + 0] = 10//(0,0,yanse)
+            pixels[px + 1] = 10//(0,0,yanse)
+            pixels[px + 2] = yanse
+            pixels[px + 3] = 100//(0,0,yanse)
+        }
+    }
+    updatePixels()
+}
+
+
